2ee34946e28f355c47f26078a7c0e66359d54fe6
Paul CACHEUX <paulcacheux@gmail.com>
2017-07-30 11:34:22 +0200

    Simplified utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,14 @@
 pub fn offset_addr(base: u32, offset: i32) -> u32 {
-    ((base as i64) + (offset as i64)) as u32
-    /*if offset >= 0 {
-        base + offset as u32
-    } else {
-        base - (-offset) as u32
-    }*/
+    let result = (base as i64) + (offset as i64);
+    let result_word = result as u32;
+    assert!(result == result_word as i64, "OVERFLOW ERROR");
+    result_word
 }
 
 pub fn u2i(input: u32) -> i32 {
-    /*unsafe {
-        std::mem::transmute::<u32, i32>(input)
-    }*/
     input as i32
 }
 
 pub fn i2u(input: i32) -> u32 {
-    /* unsafe {
-        std::mem::transmute::<i32, u32>(input)
-    }*/
     input as u32
 }
